4eb13990dacf53b873a24ad246d492fc81dc7d1c
BugPredictor <67718573+BugPredictor@users.noreply.github.com>
2025-07-14 16:37:16 +0800

    Update joint.py

--- joint.py
+++ joint.py
@@ -16,7 +16,6 @@ def train(model_f, model_g, x_train, x_train_flat, y_train, dataset_val, epochs,
     dataset_train = prepare_dataset(x_train, x_train_flat, y_train, batch_size)
     loss_fn = tf.keras.losses.CategoricalCrossentropy()
 
-    # 设置学习率调度器
     # lr_schedule = tf.keras.optimizers.schedules.ExponentialDecay(initial_learning_rate=3e-3)
     optimizer = tf.keras.optimizers.Adam(learning_rate=1e-3)
 
@@ -25,7 +24,6 @@ def train(model_f, model_g, x_train, x_train_flat, y_train, dataset_val, epochs,
 
 
     for epoch in range(epochs):
-        # 根据轮次动态调整损失函数的系数
         imbd_f = 1.4
         lmbd_g = 0.6
         lmbd_of = 0.8
@@ -61,28 +59,23 @@ def train(model_f, model_g, x_train, x_train_flat, y_train, dataset_val, epochs,
     return model_f, model_g
 
 def softmax_temperature(logits, temperature):
-    """使用特定温度参数调整 logits 并应用 softmax 得到概率分布。"""
     return tf.nn.softmax(logits / temperature)
 
 def Loss_of(f, g, x_flat, x, temperature):
     
-    # 获取预测器和解释器的 logits
     v_i_logits = f.get_logits(x)
     z_i_logits = g.get_logits(x_flat)
     
-    # 使用温度参数调整 logits 并计算概率
     q_i = softmax_temperature(z_i_logits, temperature)
     p_i = softmax_temperature(v_i_logits, temperature)
     
-    # 计算损失
     loss = -tf.reduce_sum(q_i * tf.math.log(p_i))
     return loss
 
 def Loss_fef(f, g, x_flat, x):
-    # 获取预测器和解释器的 logits
     v_i_logits = f.get_logits(x)
     z_i_logits = g.get_logits(x_flat)
-    # 计算平方差损失
+ 
     mse = tf.keras.losses.MeanSquaredError()
     loss_fef = mse(z_i_logits, v_i_logits)
     return loss_fef
@@ -110,25 +103,22 @@ def evaluate(model_f, model_g, x, x_flat, y):
 
 
 def analyze(f, g, x_test, x_test_flat, y_test):
-    # 初始化混淆矩阵
-    conf_matx_fy = np.zeros([2, 2])  # 对于f模型和真实标签y的混淆矩阵
-    conf_matx_fg = np.zeros([2, 2])  # 对于f模型和g模型的混淆矩阵，用于计算保真度
-    conf_matx_gy = np.zeros([2, 2])  # 对于g模型和真实标签y的混淆矩阵，用于计算g模型的准确度
+   
+    conf_matx_fy = np.zeros([2, 2]) 
+    conf_matx_fg = np.zeros([2, 2])  
+    conf_matx_gy = np.zeros([2, 2])  
 
-    # 对整个数据集进行预测
     output_f = f.predict(x_test)
     output_g = g.predict(x_test_flat)
     y = np.argmax(y_test, axis=1)
     pred_f = np.argmax(output_f, axis=1)
     pred_g = np.argmax(output_g, axis=1)
 
-    # 更新混淆矩阵
     for i in range(len(y)):
         conf_matx_fy[y[i], pred_f[i]] += 1
         conf_matx_fg[pred_f[i], pred_g[i]] += 1
         conf_matx_gy[y[i], pred_g[i]] += 1
 
-    # 计算准确度和保真度
     accuracy_f = np.diag(conf_matx_fy).sum() / len(y)  
     fidelity = np.diag(conf_matx_fg).sum() / len(y)  
     accuracy_g = np.diag(conf_matx_gy).sum() / len(y)  
